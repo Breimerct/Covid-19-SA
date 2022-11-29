798007bf38287dd699cd825b0f69059a25e7193b
Breimer Correa <breimerct@gmail.com>
2022-11-28 21:47:07 -0500

    add boot apexChart

--- src/boot/apexChart.ts
+++ src/boot/apexChart.ts
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers'
 import VueApexCharts from 'vue-apexcharts'
 
-export default boot(({ app, router, Vue }) => {
+export default boot(({ Vue }) => {
   Vue.use(VueApexCharts)
   Vue.component('apexChart', VueApexCharts)
 })
